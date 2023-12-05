3319b0e4bd58468a636dd284516b939e3d785edf
unel <unel@bk.ru>
2023-12-05 19:29:22 +0300

    fill some descriptions

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 
-This is a realisation of the test task
+This is a realisation of the test assigment
+
 
 For start the project type `docker compose up` in console, then open http://localhost site
+You also could try to start only frontend by `npm run idnev` command from front folder, in this case site will be available by this address: http://localhost:8080
+
+
+For developing purposes using docker variant is preferrable because this variant includes mock implementation for api. In that case you need to switch API_HOST variable in file [front/src/lib/api/coffee.ts](front/src/lib/api/coffee.ts) to API_DEV_HOST
---
+++ front/src/routes/about/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    const data = [
+        { key: "project", value: "test assignment", link: "https://github.com/unel/tt-coffee" },
+        { key: "author", value: "unel", link: "https://github.com/unel" },
+    ];
+</script>
+
+<section class="root">
+    <section class="shorts">
+        <h2>short info</h2>
+        {#each data as line}
+            <div class="line">
+                <div>{ line.key }:</div>
+
+                <div>
+                    {#if line.link}
+                        <a href={ line.link } target="_blank">
+                            { line.value }
+                        </a>
+                    {:else}
+                        { line.value }
+                    {/if}
+                </div>
+            </div>
+        {/each}
+    </section>
+
+    <section>
+        <h2>Description</h2>
+        <p>
+            This is a test project which load information about coffee flavours from api and render it.
+            There is two ways to start it: 1. by <code>docker compose up</code> command in root folder and by <code>npm run idev</code> command in front folder. 
+        </p>
+    </section>
+</section>
+
+
+<style lang="less">
+    .root {
+        padding: 16px;
+        max-width: 600px;
+    }
+    .shorts {
+        display: flex;
+        flex-direction: column;
+        row-gap: 8px;
+    }
+
+    .line {
+        display: flex;
+        flex-direction: row;
+        column-gap: 16px;
+    }
+
+    code {
+        background-color: rgba(0,0,0, .5);
+        color: lime;
+        padding: 4px;
+    }
+</style>
