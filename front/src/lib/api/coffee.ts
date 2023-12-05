b3af0f36a1ce41e9b88751e24e26c3d9c25709a7
unel <unel@bk.ru>
2023-12-05 16:10:57 +0300

    fix bug with host resolution

--- front/src/lib/api/coffee.ts
+++ front/src/lib/api/coffee.ts
@@ -1,8 +1,8 @@
 import type { TCoffee, TCoffeeIntensifier, TCoffeeRaw } from "$lib/types/tcoffee";
 
-const API_DEV_HOST = 'http://localhost';
+const API_DEV_HOST = '';
 const API_PROD_HOST = 'https://random-data-api.com';
-const API_HOST = 1 ? API_PROD_HOST : API_DEV_HOST;
+const API_HOST = 0 ? API_PROD_HOST : API_DEV_HOST;
 
 const API_BASE_URL = `${API_HOST}/api/coffee`;
 
